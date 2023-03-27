48a271b63114e68f76f2236790a89383f0d636ae
Patryk Maruda <patrykmaruda@gmail.com>
2023-03-27 22:47:38 +0200

    Comment changes

--- gtp_code_review/git_service.py
+++ gtp_code_review/git_service.py
@@ -7,20 +7,24 @@ class GitService:
 
         self.main_branch = self.repo.branches[main_branch]
         self.review_branch = self.repo.branches[review_branch] if review_branch else self.repo.active_branch
-        self.diffs = self.get_diff()
+        # self.diffs = self.get_diff()
+    #
+    # def get_diff(self):
+    #     return self.review_branch.commit.diff(self.main_branch.commit)
+    #
+    # def get_diffs_content_separated_by_file(self):
+    #     diffs_content = {}
+    #     for diff in self.diffs:
+    #         if diff.new_file:
+    #             import pdb; pdb.set_trace()
+    #             diffs_content[diff.b_path] = diff.b_blob.data_stream.read().decode('utf-8')
+    #         elif diff.deleted_file:
+    #             pass
+    #         else:
+    #             import pdb; pdb.set_trace()
+    #             diffs_content[diff.b_path] = diff.diff.decode('utf-8')
+    #
+    #     return diffs_content
 
-    def get_diff(self):
-        return self.main_branch.commit.diff(self.review_branch.commit)
-
-    def get_diffs_content_separated_by_file(self):
-        diffs_content = {}
-        for diff in self.diffs:
-            if diff.new_file:
-                diffs_content[diff.b_path] = diff.b_blob.data_stream.read().decode('utf-8')
-            elif diff.deleted_file:
-                pass
-            else:
-                diffs_content[diff.b_path] = diff.diff.decode('utf-8')
-
-        return diffs_content
-
+    def simple_diff(self):
+        return self.repo.git.diff(self.main_branch, self.repo.active_branch.name)
