6f3976330322ece4126260c86bc35b97d4f73077
Patryk Maruda <patrykmaruda@gmail.com>
2023-03-29 11:55:57 +0200

    Add tests

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gpt_code_review',
-    version='0.1',
+    version='0.1.0',
     packages=find_packages(),
     install_requires=[
         'click',
