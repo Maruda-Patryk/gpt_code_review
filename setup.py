eb1b3f4aba9fc33c43624d7bf9ceafe2756c4f6d
Patryk Maruda <patrykmaruda@gmail.com>
2023-04-04 21:03:40 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gpt_code_review',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     author='Patryk Maruda',
     author_email='patrykmaruda@gmail.com',
